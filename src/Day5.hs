c604eb30e6ff68da1579943bc3939ac4304402c6
Marc A. Saegesser <msaegesser@amientertainment.com>
2018-11-22 20:49:30 -0600

    Day 5 part 2.
    This is kind of ugly. I kept getting stack overflow exceptions. I
    think this is due to some lazy evaluation. I used the $! notation to
    cause force strictness. I finally got it to run to completion and have
    the correct answer but it took a long time to execute.

--- src/Day5.hs
+++ src/Day5.hs
@@ -12,7 +12,18 @@ executeInstruction :: Machine -> Machine
 executeInstruction (Machine is ip _) =
   let instruction     = is V.! ip
       nextIP          = ip + instruction
-      newInstructions = V.update is (V.singleton (ip, instruction+1))
+      newInstructions = V.update is $! V.singleton (ip, instruction+1)
+  in
+    case is V.!? nextIP of
+      Just _    -> Machine newInstructions nextIP Running
+      Nothing   -> Machine newInstructions nextIP Stopped
+
+executeInstruction2 :: Machine -> Machine
+executeInstruction2 (Machine is ip _) =
+  let instruction     = is V.! ip
+      nextIP          = ip + instruction
+      increment       = if instruction >= 3 then (-1) else 1
+      newInstructions = V.update is $! V.singleton (ip, instruction + increment)
   in
     case is V.!? nextIP of
       Just _    -> Machine newInstructions nextIP Running
@@ -22,8 +33,33 @@ runMachine :: Machine -> (Machine, Int)
 runMachine m =
   runner m 0
   where
-    runner mm@(Machine _ _ Running) c = runner (executeInstruction mm) (c + 1)
     runner mm@(Machine _ _ Stopped) c = (mm, c)
+    runner mm@(Machine _ _ Running) c = runner (executeInstruction mm) $! (c + 1)
+
+runMachine' :: Machine -> (Machine, Int)
+runMachine' m =
+  runner m 0
+  where
+    runner :: Machine -> Int -> (Machine, Int)
+    runner !mm !c = case mm of
+      Machine _ _ Stopped -> (mm, c)
+      Machine _ _ Running -> runner (executeInstruction mm) $! (c+1)
+
+runMachine2 :: Machine -> (Machine, Int)
+runMachine2 m =
+  runner m 0
+  where
+    runner !mm@(Machine _ _ Stopped) !c = (mm, c)
+    runner !mm@(Machine _ _ Running) !c = runner (executeInstruction2 mm) $! (c + 1)
+
+runMachine2' :: Machine -> (Machine, Int)
+runMachine2' m =
+  runner m 0
+  where
+    runner :: Machine -> Int -> (Machine, Int)
+    runner !mm !c = case mm of
+      Machine _ _ Stopped -> (mm, c)
+      Machine _ _ Running -> runner (executeInstruction2 mm) $! (c+1)
 
 withInput :: FilePath -> IO Machine
 withInput path = do
@@ -37,3 +73,5 @@ day05 =
   withInput "data/Day5.txt" >>= \machine -> do
     putStrLn "Part 1"
     print $ snd $ runMachine machine
+    putStrLn "Part 2"
+    print $ snd $ runMachine2 machine
